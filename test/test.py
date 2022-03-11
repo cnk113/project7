d5eff6c980764e0b4bcf1d258d8925555423c4a0
Chang Kim <changkim@Changs-MBP-2.ucsfmedicalcenter.org>
2022-03-10 16:30:19 -0800

    further cleanup

--- test/test.py
+++ test/test.py
@@ -5,39 +5,6 @@ from sklearn import datasets
 from nn import preprocess, one_hot_encode_seqs 
 from collections import Counter
 import numpy as np
-# TODO: Write your test functions and associated docstrings below.
-
-def test_forward():
-    pass
-
-
-def test_single_forward():
-    pass
-
-
-def test_single_backprop():
-    pass
-
-
-def test_predict():
-    pass
-
-
-def test_binary_cross_entropy():
-    pass
-
-
-def test_binary_cross_entropy_backprop():
-    pass
-
-
-def test_mean_squared_error():
-    pass
-
-
-def test_mean_squared_error_backprop():
-    pass
-
 
 def test_one_hot_encode():
     '''
